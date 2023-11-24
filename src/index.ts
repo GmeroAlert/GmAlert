71df92cbecb656449a78c3dc56fa1ec1ae1212ee
gmero <gmero@foxmail.com>
2023-11-24 21:30:37 +0800

    优化api调用方式

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,43 @@
-import type { MsgPropsExt } from './modules/Msg/Msg'
+import type { MsgPropsFull } from './modules/Msg/Msg'
 import { Msg } from './modules/Msg/Msg'
 
 import './styles/global.scss'
 
+function getArgs(args: (string | object)[]) {
+  const result: MsgPropsFull = {
+    content: 'success',
+    type: 'success',
+  }
+
+  let firstStr = false
+  const assignArg = (arg: string | object) => {
+    switch (typeof arg) {
+      case 'string':
+        if (firstStr) {
+          result.type = arg as MsgPropsFull['type']
+        } else {
+          result.content = arg
+          firstStr = true
+        }
+        break
+      case 'object':
+        Object.assign(result, arg)
+        break
+    }
+  }
+
+  for (let index = 0; index < 3; index++) {
+    const element = args[index]
+    element && assignArg(element)
+  }
+
+  return result
+}
+
 function MakeMsg(form: number) {
   const $msg = new Msg(form)
-  const res = (
-    content: string,
-    type?: 'success' | 'error' | 'warning' | 'info' | 'loading',
-    conf?: MsgPropsExt,
-  ) => {
-    return $msg.fire(content, type, conf)
+  const res = (...args: string[]) => {
+    return $msg.fire(getArgs(args))
   }
 
   res.config = $msg.config.bind($msg)
