5c495ff104f5095f2eb944931e4f472bb81d8b36
gmero <gmero@foxmail.com>
2023-12-03 23:46:32 +0800

    Update package.json and rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -24,12 +24,21 @@ const bundles = [
     },
   },
   {
+    input: './src/index.ts',
+    output: {
+      file: `./dist/${libName}-bundle.min.js`,
+      format: 'iife',
+      name: funcName,
+      sourcemap: false,
+    },
+  },
+  {
     input: './src/main.ts',
     output: {
       file: `./dist/${libName}.esm.js`,
       format: 'esm',
     },
-  }
+  },
 ]
 
 export default bundles.map(({ input, output }) => ({
@@ -40,17 +49,18 @@ export default bundles.map(({ input, output }) => ({
       extensions: ['.js', '.jsx', '.ts', '.tsx'],
     }),
     postcss({
-      plugins: [postcssPresetEnv()],
       minimize: true,
-      extract: `${libName}.min.css`, // 如果你想导出css而不是css in js
+      extract: output.file.includes('-bundle')? false:`${libName}.min.css`, // 如果你想导出css而不是css in js
+      inject: output.file.includes('-bundle'),
+      plugins: [postcssPresetEnv()],
     }),
     commonjs(),
     babel({
       babelHelpers: 'bundled',
       extensions: ['.js', '.jsx', '.ts', '.tsx'],
       exclude: 'mode_modules/**',
-      plugins: ['annotate-pure-calls']
+      plugins: ['annotate-pure-calls'],
     }),
     output.file.includes('.min.') && terser(),
-  ]
+  ],
 }))
