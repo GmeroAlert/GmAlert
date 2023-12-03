87981fec7eeae3a2d2f4fa98b530caaab693c585
gmero <gmero@foxmail.com>
2023-12-04 02:57:54 +0800

    Fix animation timing in notice module

--- src/modules/notice.ts
+++ src/modules/notice.ts
@@ -13,35 +13,22 @@ export function GmNotice(props: PropsMessage): MsgType {
   $wrapper.innerHTML = `<div class="${cn('notice-main')}">${icon}\
   <div class="${cn('notice-content')}">${props.content}</div></div>`
 
-  animationendHandle($wrapper, (animationName) => {
-    if (animationName === cn('open')) {
-      changeStyle($wrapper, [
-        `opacity:1`,
-        `animation-name:${cn('notice-movein')}`,
-      ])
-    }
-
-    if (animationName === cn('notice-moveout')) {
-      changeAnimation($wrapper, cn('close'))
-    }
-  })
-
   const open = () => {
     getNoticeContainer().prepend($wrapper)
-    changeStyle($wrapper, [`max-height:${$wrapper.offsetHeight + 10}px`])
+    changeStyle($wrapper, [`--mh:${$wrapper.offsetHeight + 10}px`])
     changeAnimation($wrapper, cn('open'))
     setTimeout(() => {
       const $icon = $wrapper.querySelector<HTMLElement>(`.${cn('notice-icon')}`)
       if ($icon) {
         $icon.style.opacity = '1'
       }
-    }, 400)
+    }, 300)
   }
 
   const close = (status: number) => {
     props.onClose()
+    changeAnimation($wrapper, cn('close'))
     return new Promise<void>((resolve) => {
-      changeAnimation($wrapper, cn('notice-moveout'))
       animationendHandle($wrapper, (animationName) => {
         if (animationName === cn('close')) {
           $wrapper.remove()
